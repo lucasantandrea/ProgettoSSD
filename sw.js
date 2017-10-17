99a5c0dc79978ead738bdce2c9c57cc20db24761
lucasantandrea <santandrea.luca@gmail.com>
2017-10-17 19:19:03 +0200

    update cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // utilizzo cacheName per cache versioning
-var cacheName = 'v4:static';
+var cacheName = 'v5:static';
 
 // installazione: metto in cache elementi statici (per funzionamento offline)
 self.addEventListener('install', function(e) {
